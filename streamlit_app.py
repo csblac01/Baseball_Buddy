b87c5b4c5c643db743d7b1c32f6163068798c18a
csblac01 <146751671+csblac01@users.noreply.github.com>
2026-01-07 16:13:06 +0000

    Merging app.py with streamlit app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+
+st.set_page_config(page_title="Baseball App", layout="wide")
+st.title("⚾ Baseball App")
+st.write("It works! Next: add features + data.")
